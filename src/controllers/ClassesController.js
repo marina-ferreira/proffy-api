8dfcdbc5ae657e8240221d5547a2b2e07220d9f3
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-06 18:23:26 -0300

    Classes controller

---
+++ src/controllers/ClassesController.js
@@ -0,0 +1,52 @@
+const db = require('../database/connection.js')
+const timeConverter = require('../utils/timeConverter.js')
+
+class ClassesController {
+  async create(request, response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule
+    } = request.body
+
+    const trx = await db.transaction()
+
+    try {
+      const userIds = await trx('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+      })
+
+      const classIds = await trx('classes').insert({
+        subject,
+        cost,
+        user_id: userIds[0]
+      })
+
+      const classSchedulesParams = schedule.map(item => ({
+        week_day: item.week_day,
+        from: timeConverter(item.from),
+        to: timeConverter(item.to),
+        class_id: classIds[0]
+      }))
+
+      await trx('class_schedule').insert(classSchedulesParams)
+
+      await trx.commit()
+
+      return response.status(201).json()
+    } catch(error) {
+      console.log(error)
+      await trx.rollback()
+      return response.status(400).json({ message: 'Error on Class creation' })
+    }
+  }
+}
+
+export default ClassesController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,54 +1,11 @@
 const express = require('express')
 
-const db = require('../database/connection.js')
-const timeConverter = require('../utils/timeConverter.js')
+const ClassesController = require('../controllers/ClassesController')
 
 const routes = express.Router()
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body
+const classesController = new ClassesController()
 
-  const trx = await db.transaction()
-
-  try {
-    const userIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    })
-
-    const classIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id: userIds[0]
-    })
-
-    const classSchedulesParams = schedule.map(item => ({
-      week_day: item.week_day,
-      from: timeConverter(item.from),
-      to: timeConverter(item.to),
-      class_id: classIds[0]
-    }))
-
-    const classSchedules = await trx('class_schedule').insert(classSchedulesParams)
-
-    await trx.commit()
-
-    return response.status(201).json()
-  } catch(error) {
-    console.log(error)
-    await trx.rollback()
-    return response.status(400).json({ message: 'Error on Class creation' })
-  }
-})
+routes.post('/classes', classesController.create)
 
 module.exports = routes
