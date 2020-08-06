32f7622cdd4283a26f3b1358a1746f374f9f7b30
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-05 22:37:32 -0300

    Create users migrations

---
+++ src/database/migrations/20200805222225_CreateUsers.js
@@ -0,0 +1,14 @@
+
+exports.up = async function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.string('avatar').notNullable()
+    table.string('whatsapp').notNullable()
+    table.string('bio').notNullable()
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+}
